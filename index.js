c4e80cf39c502afad8bd333d3797fe2bcd99c5eb
Jacob Carver <jake10693@gmail.com>
2020-06-24 17:05:36 -0700

    Trying new solution recomendation, installed fs, path, and inquirer.

--- index.js
+++ index.js
@@ -1,132 +1,72 @@
-const fs = require ('fs');
-const questions = ["Project title?", "Description?", "Table of Contents?", "Installation?", "Usage?", "License?", "Contributing?", "Tests?", "Questions?", "User GitHub profile picture?", "User GitHub email?"];
-// const prompts = require('prompts');
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-// function askQuestions(){
-//     for (var i = 0; i < questions.length; i++) {
-//         rl.question(questions[i]);
-//     }}
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-//     fs.writeFile('readme.txt', questions.length, askQuestions)
-// rl.question("What is your github username? ", function(username) {
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?"
+  },
+  {
+    type: "input",
+    name: "URL to Project",
+    message: "the URL to your project?"
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project's name?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project"
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What command should be run to install dependencies?",
+    default: "npm i"
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be run to run tests?",
+    default: "npm test"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about using the repo?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "What does the user need to know about contributing to the repo?",
+  }
+];
 
-    rl.question("What is the title of your project? ", function(name) { 
-        rl.question("Give a brief description of the project. ", function(description) {
-            rl.question("Table of Contents ", function(ToC) {
-                rl.question("Installaion ", function(Installation) {
-                    rl.question("Usage ", function(Usage) {
-                        rl.question("License ", function(License) {
-                            rl.question("Contributing ", function(Contributing) {
-                                rl.question("Tests ", function(Tests) {
-                                    rl.question("Questions ", function(Questions) {
-                                        
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
+function init() {
+  inquirer.prompt(questions).then((inquirerResponses) => {
+    writeToFile("README.md", generateMarkdown({ ...inquirerResponses }));
+  })
+}
 
-                                            fs.writeFile('readme.md',{name}, (err) => {
-                                                // if (err) throw err;
-                                                // console.log('The lyrics were updated!');
-                                            });
-                                            fs.appendFile('readme.md',{description}, (err) => {
-                                                if (err) throw err;
-                                                console.log('The lyrics were updated!');
-                                            });
-                                            fs.appendFile('readme.text',{ToC}, (err) => {
-                                                if (err) throw err;
-                                                console.log('The lyrics were updated!');
-                                            });
-                                            fs.appendFile('readme.text', {Installation}, (err) => {
-                                                if (err) throw err;
-                                                console.log('The lyrics were updated!');
-                                            });
-                                            fs.appendFile('readme.text', {Usage}, (err) => {
-                                                if (err) throw err;
-                                                console.log('The lyrics were updated!');
-                                            });
-                                            fs.appendFile('readme.text', {License}, (err) => {
-                                                if (err) throw err;
-                                                console.log('The lyrics were updated!');
-                                            });
-                                            fs.appendFile('readme.text', {Contributing}, (err) => {
-                                                if (err) throw err;
-                                                console.log('The lyrics were updated!');
-                                            });
-                                            fs.appendFile('readme.text', {Tests}, (err) => {
-                                                if (err) throw err;
-                                                console.log('The lyrics were updated!');
-                                            });
-                                            fs.appendFile('readme.text', {Questions}, (err) => {
-                                                if (err) throw err;
-                                                console.log('The lyrics were updated!');
-                                            });
-                                            rl.close();
-
-                                    });
-                                });
-                            });
-                        });
-                    });    
-                });        
-            });    
-        });
-    });
-
-
-    rl.on("close", function() {
-        console.log("\nBYE BYE !!!");
-        process.exit(0);
-    });
-// 'use strict';
-
-// const { prompt } = require('./');
-
-// let interval;
-
-// (async function(){
-//     const questions = [
-//         {
-//             type: 'text',
-//             name: 'twitter',
-//             message: `What's your twitter handle?`,
-//             initial: `terkelg`,
-//             format: v => `@${v}`
-//         },
-//         {
-//             type: 'number',
-//             name: 'age',
-//             message: 'How old are you?',
-//             validate: value => value < 18 ? `Sorry, you have to be 18` : true
-//         },
-//         {
-//             type: 'password',
-//             name: 'secret',
-//             message: 'Tell me a secret'
-//         },
-//         {
-//             type: 'confirm',
-//             name: 'confirmed',
-//             message: 'Can you confirm?'
-//         },
-//         {
-//             type: prev => prev && 'toggle',
-//             name: 'confirmtoggle',
-//             message: 'Can you confirm again?',
-//             active: 'yes',
-//             inactive: 'no'
-//         },
-//         {
-//             type: 'list',
-//             name: 'keywords',
-//             message: 'Enter keywords'
-//         }
-//     ]
-
-//         const answers = await prompt(questions, {onCancel:cleanup, onSubmit:cleanup});
-
-        
-
-// })
-  
+init();
